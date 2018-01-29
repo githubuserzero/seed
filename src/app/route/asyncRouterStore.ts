61eac845dea1c653d2f9902cf523171a59563150
SSen <i@ssen.name>
2018-01-29 18:44:22 +0900

    Fixed to enable both dynamic import and default export

--- src/app/route/asyncRouterStore.ts
+++ src/app/route/asyncRouterStore.ts
@@ -4,10 +4,10 @@ export default new AsyncRouterStore([
   {
     path: '/',
     exact: true,
-    component: () => import('app/router-components/main'),
+    component: () => System.import('app/router-components/main'),
   },
   {
     path: '/sample',
-    component: () => import('app/router-components/sample'),
+    component: () => System.import('app/router-components/sample'),
   },
 ]);
