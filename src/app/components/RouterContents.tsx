6c53a43dbb7999bf4491cf0a4847240974727d0c
SSen <i@ssen.name>
2019-03-23 00:24:06 +0900

    feat: Add react-router-store's `<Suspense/>` to RouterContents

--- src/app/components/RouterContents.tsx
+++ src/app/components/RouterContents.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { RouteStore } from 'react-router-store';
+import { Switch } from 'react-router-dom';
+import { RouteStore, Suspense } from 'react-router-store';
 
 export interface RouterContentsProps {
   routeStore: RouteStore;
 }
 
+function LoadingMessage() {
+  return <div>Loading...</div>;
+}
+
 export function RouterContents({routeStore}: RouterContentsProps) {
   return (
-    <>
-      {routeStore.getAllRoutes()}
-    </>
+    <Suspense fallback={<LoadingMessage/>}>
+      <Switch>
+        {routeStore.getAllRoutes()}
+      </Switch>
+    </Suspense>
   );
 }
